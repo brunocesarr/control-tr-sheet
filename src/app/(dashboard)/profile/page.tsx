f678739fe6aeacbf9e00e44aae0b7b0286e1e241
bruno.cesar <bruno.cesar@dtidigital.com.br>
2025-01-05 22:12:27 -0300

    #feat: Add toast.

--- src/app/(dashboard)/profile/page.tsx
+++ src/app/(dashboard)/profile/page.tsx
@@ -9,9 +9,12 @@ import { GoAlertFill } from 'react-icons/go';
 import { redirect } from 'next/navigation';
 import { AlertModal, ConfirmModal, InputModal } from '@/components/CustomModals';
 import { validateEmail, validateName, validatePassword } from '@/helpers/validators';
+import { Bounce, toast, ToastContainer } from 'react-toastify';
+import Loader from '@/components/Loader';
 
 export default function Profile() {
-  const { loggedInUser, logout, updateName, updateEmail, updatePassword } = useContext(AuthContext);
+  const { loggedInUser, logout, updateName, updateEmail, updatePassword, isLoading } =
+    useContext(AuthContext);
 
   const [newName, setNewName] = useState('');
   const [oldPassword, setOldPassword] = useState('');
@@ -40,6 +43,9 @@ export default function Profile() {
       }
 
       await updateName(newName);
+      setTimeout(() => {
+        toast.success('Alteracao realizada com sucesso.');
+      }, 1000);
     } catch (error) {
       console.error(error);
       setErrorMessage('Erro ao realizar a alteracao do nome.');
@@ -56,6 +62,9 @@ export default function Profile() {
       }
 
       await updateEmail(newEmail);
+      setTimeout(() => {
+        toast.success('Alteracao realizada com sucesso.');
+      }, 1000);
     } catch (error) {
       console.error(error);
       setErrorMessage('Erro ao realizar a alteracao do email.');
@@ -72,6 +81,9 @@ export default function Profile() {
       }
 
       await updatePassword(oldPassword, newPassword);
+      setTimeout(() => {
+        toast.success('Alteracao realizada com sucesso.');
+      }, 1000);
     } catch (error) {
       console.error(error);
       setErrorMessage('Erro ao realizar a alteracao da senha.');
@@ -83,114 +95,121 @@ export default function Profile() {
     return redirect('/login');
   }
 
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
     <div>
       <Navbar>
         <p>Configuracoes de conta</p>
       </Navbar>
-      <div className="px-4 h-full w-full">
-        <div className="flex flex-row items-center justify-between px-2">
-          <h1 className="border-b py-6 text-4xl font-semibold">Bem vindo, {loggedInUser.name}</h1>
-          <button
-            type="button"
-            className="flex items-center justify-center gap-2 w-fit min-h-[48px] p-4 cursor-pointer rounded-md transition center text-white bg-red-900 hover:bg-red-800 hover:scale-105"
-            onClick={() => setOpenConfirmModal(true)}>
-            <PiSignOut /> Sair
-          </button>
-        </div>
-        <div className="grid grid-cols-8 pt-3 sm:grid-cols-10">
-          <div className="col-span-12 overflow-hidden rounded-xl sm:bg-gray-50 sm:px-8 sm:shadow">
-            <p className="py-2 text-xl font-semibold">Endereco de Email</p>
-            <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
-              <p className="text-gray-600">
-                Seu email é <strong>{loggedInUser.email}</strong>
-              </p>
+      {isLoading && <Loader />}
+      {!isLoading && (
+        <div className="px-4 h-full w-full">
+          <div className="flex flex-row items-center justify-between px-2">
+            <h1 className="border-b py-6 text-4xl font-semibold">Bem vindo, {loggedInUser.name}</h1>
+            <button
+              type="button"
+              className="flex items-center justify-center gap-2 w-fit min-h-[48px] p-4 cursor-pointer rounded-md transition center text-white bg-red-900 hover:bg-red-800 hover:scale-105"
+              onClick={() => setOpenConfirmModal(true)}>
+              <PiSignOut /> Sair
+            </button>
+          </div>
+          <div className="grid grid-cols-8 pt-3 sm:grid-cols-10">
+            <div className="col-span-12 overflow-hidden rounded-xl sm:bg-gray-50 sm:px-8 sm:shadow">
+              <p className="py-2 text-xl font-semibold">Endereco de Email</p>
+              <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
+                <p className="text-gray-600">
+                  Seu email é <strong>{loggedInUser.email}</strong>
+                </p>
+                <button
+                  onClick={() => setOpenInputModal(true)}
+                  className="inline-flex text-sm font-semibold text-blue-600 underline decoration-2">
+                  Alterar
+                </button>
+              </div>
+              <hr className="mt-4 mb-8" />
+              <p className="py-2 text-xl font-semibold">Nome</p>
+              <div className="flex items-center">
+                <div className="flex flex-col space-y-2 sm:flex-row sm:space-y-0 sm:space-x-3">
+                  <label>
+                    <span className="text-sm text-gray-500">Insira o novo nome</span>
+                    <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
+                      <input
+                        type="text"
+                        className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
+                        placeholder={loggedInUser.name}
+                        value={newName}
+                        onChange={(e) => setNewName(e.target.value)}
+                      />
+                    </div>
+                  </label>
+                </div>
+              </div>
               <button
-                onClick={() => setOpenInputModal(true)}
-                className="inline-flex text-sm font-semibold text-blue-600 underline decoration-2">
-                Alterar
+                disabled={!newName}
+                onClick={handleNewNome}
+                className="mt-4 rounded-lg bg-gray-600 px-4 py-2 text-white">
+                Salvar novo nome
               </button>
-            </div>
-            <hr className="mt-4 mb-8" />
-            <p className="py-2 text-xl font-semibold">Nome</p>
-            <div className="flex items-center">
-              <div className="flex flex-col space-y-2 sm:flex-row sm:space-y-0 sm:space-x-3">
-                <label>
-                  <span className="text-sm text-gray-500">Insira o novo nome</span>
-                  <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
-                    <input
-                      type="text"
-                      className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
-                      placeholder={loggedInUser.name}
-                      value={newName}
-                      onChange={(e) => setNewName(e.target.value)}
-                    />
-                  </div>
-                </label>
+              <hr className="mt-4 mb-8" />
+              <p className="py-2 text-xl font-semibold">Senha</p>
+              <div className="flex items-center">
+                <div className="flex flex-col space-y-2 sm:flex-row sm:space-y-0 sm:space-x-3">
+                  <label>
+                    <span className="text-sm text-gray-500">Insira a senha antiga</span>
+                    <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
+                      <input
+                        type="password"
+                        className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
+                        placeholder="********"
+                        value={oldPassword}
+                        onChange={(e) => setOldPassword(e.target.value)}
+                      />
+                    </div>
+                  </label>
+                  <label>
+                    <span className="text-sm text-gray-500">Insira a senha nova</span>
+                    <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
+                      <input
+                        type="password"
+                        className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
+                        placeholder="********"
+                        value={newPassword}
+                        onChange={(e) => setNewPassword(e.target.value)}
+                      />
+                    </div>
+                  </label>
+                </div>
               </div>
-            </div>
-            <button
-              disabled={!newName}
-              onClick={handleNewNome}
-              className="mt-4 rounded-lg bg-gray-600 px-4 py-2 text-white">
-              Salvar novo nome
-            </button>
-            <hr className="mt-4 mb-8" />
-            <p className="py-2 text-xl font-semibold">Senha</p>
-            <div className="flex items-center">
-              <div className="flex flex-col space-y-2 sm:flex-row sm:space-y-0 sm:space-x-3">
-                <label>
-                  <span className="text-sm text-gray-500">Insira a senha antiga</span>
-                  <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
-                    <input
-                      type="password"
-                      className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
-                      placeholder="********"
-                      value={oldPassword}
-                      onChange={(e) => setOldPassword(e.target.value)}
-                    />
-                  </div>
-                </label>
-                <label>
-                  <span className="text-sm text-gray-500">Insira a senha nova</span>
-                  <div className="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
-                    <input
-                      type="password"
-                      className="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none"
-                      placeholder="********"
-                      value={newPassword}
-                      onChange={(e) => setNewPassword(e.target.value)}
-                    />
-                  </div>
-                </label>
-              </div>
-            </div>
-            <button
-              disabled={!newPassword || !oldPassword}
-              onClick={handleNewPassword}
-              className="mt-4 rounded-lg bg-gray-600 px-4 py-2 text-white">
-              Salvar nova senha
-            </button>
-            <hr className="mt-4 mb-8" />
+              <button
+                disabled={!newPassword || !oldPassword}
+                onClick={handleNewPassword}
+                className="mt-4 rounded-lg bg-gray-600 px-4 py-2 text-white">
+                Salvar nova senha
+              </button>
+              <hr className="mt-4 mb-8" />
 
-            <div className="mb-10 hidden">
-              <p className="py-2 text-xl font-semibold">Apagar conta</p>
-              <p className="inline-flex items-center rounded-full bg-rose-100 px-4 py-1 text-rose-600">
-                <GoAlertFill className="text-rose-600 mr-2" />
-                Prossiga com cuidado
-              </p>
-              <p className="mt-2">
-                Certifique-se de ter feito backup de sua conta caso precise obter acesso aos seus
-                dados. Limparemos completamente seus dados. Não há como acessar seu conta após esta
-                ação.
-              </p>
-              <button className="ml-auto text-sm font-semibold text-rose-600 underline decoration-2">
-                Continue com a exclusao da conta
-              </button>
+              <div className="mb-10 hidden">
+                <p className="py-2 text-xl font-semibold">Apagar conta</p>
+                <p className="inline-flex items-center rounded-full bg-rose-100 px-4 py-1 text-rose-600">
+                  <GoAlertFill className="text-rose-600 mr-2" />
+                  Prossiga com cuidado
+                </p>
+                <p className="mt-2">
+                  Certifique-se de ter feito backup de sua conta caso precise obter acesso aos seus
+                  dados. Limparemos completamente seus dados. Não há como acessar seu conta após
+                  esta ação.
+                </p>
+                <button className="ml-auto text-sm font-semibold text-rose-600 underline decoration-2">
+                  Continue com a exclusao da conta
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
       <AlertModal open={openAlertModal} setOpen={setOpenAlertModal} errorMessage={errorMessage} />
       <InputModal
         open={openInputModal}
@@ -203,6 +222,19 @@ export default function Profile() {
         setOpen={setOpenConfirmModal}
         modalDescription={'Deseja realmente sair?'}
         confirmAction={handleLogout}
+      />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
       />
     </div>
   );
